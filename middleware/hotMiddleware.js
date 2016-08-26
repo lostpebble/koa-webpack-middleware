62cea1b9147fab5af8248ed8fdd102db2c358052
Paul Myburgh <paulmyburgh88@gmail.com>
2016-08-26 12:48:19 +0200

    changed package to allow preinstall for git dependency

--- middleware/hotMiddleware.js
+++ middleware/hotMiddleware.js
@@ -2,9 +2,9 @@ import hotMiddleware from 'webpack-hot-middleware'
 import { PassThrough } from 'stream'
 
 export default (compiler, opts) => {
-  const expressMiddleware = hotMiddleware(compiler, opts)
+  const expressMiddleware = hotMiddleware(compiler, opts);
   return async (ctx, next) => { // eslint-disable-line
-    let stream = new PassThrough()
+    let stream = new PassThrough();
     ctx.body = stream
     await expressMiddleware(ctx.req, {
       write: stream.write.bind(stream),
